7503726f1589aa1ae1f70feecc49c440cf043703
gusemil <gusemil@hotmail.com>
2024-07-09 18:18:11 +0300

    Update score after a game test

--- Program.cs
+++ Program.cs
@@ -2,15 +2,35 @@
 {
     static void Main(string[] args)
     {
-        float player_1_score = 1000;
-        float player_2_score = 2000;
+        //Algorithm help: https://www.geeksforgeeks.org/elo-rating-algorithm/
 
-        //Probablity
-        double p1 = 1.0f / (1.0f + Math.Pow(10, ((player_1_score - player_2_score) / 400))); //400 * wins - losses = 400 * 1
-        double p2 = 1.0f / (1.0f + Math.Pow(10, ((player_2_score - player_1_score) / 400))); //400 * wins - losses = 400 * 1
+        float player_1_score = 1000;
+        float player_2_score = 1200;
+
+        int amountOfGamesPlayed = 1;
+
+        //Propability
+        double p1 = (1.0f / (1.0f + Math.Pow(10, ((player_1_score - player_2_score) / 400)))) / amountOfGamesPlayed; //400 * wins - losses = 400 * 1
+        double p2 = (1.0f / (1.0f + Math.Pow(10, ((player_2_score - player_1_score) / 400)))) / amountOfGamesPlayed; //400 * wins - losses = 400 * 1
 
         Console.WriteLine("Player 1 propability " + p1.ToString());
         Console.WriteLine("Player 2 propability: " + p2.ToString());
+
+        //Update ranking
+
+        //Algorithm help: https://www.omnicalculator.com/sports/elo
+
+        //FIDE uses k-factor of 40 for players under 2300 ELO and less than 30 games: https://en.wikipedia.org/wiki/Elo_rating_system#Most_accurate_K-factor
+        //20 for over 30 games but under 2400 ELO
+        //10 if 2400 or over ELO and at least 30 games
+        int k = 40;
+
+        //Player 1 win test
+        player_1_score += k * (1 - (float)p2); //1 win - 0 losses = 1
+        player_2_score += k * ((float)p2 - 1); //0 wins - 1 losses = -1
+
+        Console.WriteLine("player 1 new ELO rating: " + player_1_score);
+        Console.WriteLine("player 2 new ELO rating: " + player_2_score);
 
     }
 }
