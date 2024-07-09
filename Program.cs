f4244aee3b48a3f8ad92a21d8c1167300804028a
gusemil <gusemil@hotmail.com>
2024-07-09 17:36:48 +0300

    Win chance propability

--- Program.cs
+++ Program.cs
@@ -2,8 +2,15 @@
 {
     static void Main(string[] args)
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("Hello, World!");
+        float player_1_score = 1000;
+        float player_2_score = 2000;
+
+        //Probablity
+        double p1 = 1.0f / (1.0f + Math.Pow(10, ((player_1_score - player_2_score) / 400))); //400 * wins - losses = 400 * 1
+        double p2 = 1.0f / (1.0f + Math.Pow(10, ((player_2_score - player_1_score) / 400))); //400 * wins - losses = 400 * 1
+
+        Console.WriteLine("Player 1 propability " + p1.ToString());
+        Console.WriteLine("Player 2 propability: " + p2.ToString());
 
     }
 }
